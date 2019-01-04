7daa7caab5360e6d4f722d362e479424e95453c1
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-04 18:57:30 +0100

    Add a few links to the frontend
    Change-Id: I3002b8fce4b3a8f0b26ad280195e55869abe9fe9

--- full_stack_example/frontend/client-app/src/app/app-routing.module.ts
+++ full_stack_example/frontend/client-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FoobarComponent } from './foobar/foobar.component';
+import { LoremipsumComponent } from './loremipsum/loremipsum.component';
 
 const routes: Routes = [
     { path: 'foobar', component: FoobarComponent },
+    { path: 'loremipsum', component: LoremipsumComponent },
 ];
 
 @NgModule({
