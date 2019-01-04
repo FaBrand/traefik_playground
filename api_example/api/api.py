0454a2050b138fd1f7e0d120f3bf5e6d85ddfd6c
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-04 17:34:25 +0100

    Make api return headers it received
    Change-Id: I956c53a6e4d859429da42a910736f9d5c9ceb4a5

--- api_example/api/api.py
+++ api_example/api/api.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return jsonify({'reached': 'root'})
+    return jsonify({'reached': 'root', 'headers': dict(request.headers)})
 
 @app.route("/api")
 def api():
-    return jsonify({'reached': 'api'})
+    return jsonify({'reached': 'api', 'headers': dict(request.headers)})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
