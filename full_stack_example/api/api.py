5552431cbbcfc49667c6507a5d2eb23db3eb4f39
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-04 23:20:35 +0100

    Make api provide some random numbers
    Change-Id: I4ce6c98b6a98be76823e803c1bcb43ffe510b2de

--- full_stack_example/api/api.py
+++ full_stack_example/api/api.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, jsonify, request
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -9,6 +10,10 @@ def root():
 @app.route("/api")
 def api():
     return jsonify({'reached': 'api', 'headers': dict(request.headers)})
+
+@app.route("/api/data")
+def api_data():
+    return jsonify({'data': [random.randint(1, 100) for _ in range(1,50)]})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
