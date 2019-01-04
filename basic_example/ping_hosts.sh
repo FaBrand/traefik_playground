bbcf6124cb0754aad703042dab0d08a0ed6627fd
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-04 16:58:50 +0100

    tweak some things
    Change-Id: Ib2a7e708fbd74f31b556065700ec7457aeafd292

--- basic_example/ping_hosts.sh
+++ basic_example/ping_hosts.sh
@@ -2,10 +2,10 @@
 
 echo "================================"
 echo "Pinging whoami1"
-curl -X GET -H 'Host: whoami.server.test' localhost -L | grep -e '^IP' -A15 -B15
+curl -X GET -H 'Host: whoami.server.test' localhost -L | grep -e '^IP: 172.*' -A15 -B15
 echo "================================"
 
 echo "================================"
 echo "Pinging whoami2"
-curl -X GET -H 'Host: iamme.server.test' localhost -L | grep -e '^IP' -A15 -B15
+curl -X GET -H 'Host: iamme.server.test' localhost -L | grep -e '^IP: 172.*' -A15 -B15
 echo "================================"
