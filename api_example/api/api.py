2655b498198b9da569fa7c5528fa6daaf839c110
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-04 17:11:42 +0100

    fix typo
    Change-Id: Iaaccb8aa6b0cded829a773e768b94a7ef8b33f15

--- api_example/api/api.py
+++ api_example/api/api.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify({'status': true})
+    return jsonify({'status': True})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
