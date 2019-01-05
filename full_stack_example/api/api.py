2bcde9ad6720336a78e1cd20e3be5324f1a6f7ff
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-05 02:47:55 +0100

    Add postgres layer (uninitialized in backend)
    Change-Id: I3eff60f5d90bd88e4554faff42f6d291244d2d59

--- full_stack_example/api/api.py
+++ full_stack_example/api/api.py
@@ -1,9 +1,21 @@
+import os
 import random
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+class Config(object):
+     SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-really-secret-key'
+     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://testusr:password@postgres:5432/testdb'
+     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 app = Flask(__name__)
+app.config.from_object(Config)
 app.url_map.strict_slashes = False
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def root():
