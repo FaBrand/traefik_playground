9256a1a0a3608f19e64f50e665ad733567a85d8d
21087362+FaBrand@users.noreply.github.com <21087362+FaBrand@users.noreply.github.com>
2019-01-05 16:01:25 +0100

    Add a basic table
    Change-Id: I5d6985acfc8dfadea6d1a38432dc910d583fd530

--- full_stack_example/api/api.py
+++ full_stack_example/api/api.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 
 class Config(object):
      SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-really-secret-key'
-     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://testusr:password@postgres:5432/testdb'
+     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///:memory:')
      SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
@@ -16,6 +16,10 @@ app.url_map.strict_slashes = False
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+class DataEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.Integer)
 
 @app.route("/")
 def root():
@@ -27,7 +31,8 @@ def api():
 
 @app.route("/api/data")
 def api_data():
-    return jsonify({'data': [random.randint(1, 100) for _ in range(1,50)]})
+    all_data = DataEntry.query.all()
+    return jsonify({'data': [d.data for d in all_data]})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
